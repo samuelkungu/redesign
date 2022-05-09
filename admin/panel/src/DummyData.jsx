55df5c1b111cf5fd53fe311c969badfcaf8fa254
Samuel <kungusamuel64@gmail.com>
2022-05-09 14:56:13 +0300

    user chart

---
+++ admin/panel/src/DummyData.jsx
@@ -0,0 +1,63 @@
+export const userData = [
+    { 
+        name: 'Jan',
+        "Active Users": 2000, 
+    },
+
+    { 
+        name: 'Feb',
+        "Active Users": 5700, 
+    },
+
+    { 
+        name: 'Mar',
+        "Active Users": 3200, 
+    },
+
+    { 
+        name: 'Apr',
+        "Active Users": 2800, 
+
+    },
+
+    { 
+        name: 'May',
+        "Active Users": 4400, 
+    },
+
+    { 
+        name: 'Jun',
+        "Active Users": 2100, 
+    },
+
+    { 
+        name: 'Jul',
+        "Active Users": 7100, 
+    },
+
+    { 
+        name: 'Aug',
+        "Active Users": 4000, 
+    },
+
+    { 
+        name: 'Sep',
+        "Active Users": 6000, 
+    },
+
+    { 
+        name: 'Oct',
+        "Active Users": 5500, 
+    },
+
+    { 
+        name: 'Nov',
+        "Active Users": 4900, 
+    },
+
+    { 
+        name: 'Dec',
+        "Active Users": 7000, 
+    },
+
+  ];
--- admin/panel/src/pages/home/Home.jsx
+++ admin/panel/src/pages/home/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import Chart from '../../components/chart/Chart';
 import Featuredinfo from '../../components/featuredInfo/Featuredinfo';
 import "./home.css";
+import { userData } from '../../DummyData';
 
 function Home() {
   return (
    <div className="home">
 
       <Featuredinfo/>
+      <Chart data={userData} dataKey="Active Users" title="User Analytics" grid/>
       
    </div>
   )
