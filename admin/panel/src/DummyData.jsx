fb3d9730d2584d2054893654d0040b98273576f3
Samuel <kungusamuel64@gmail.com>
2022-05-10 17:42:31 +0300

    data

---
+++ admin/panel/src/DummyData.jsx
@@ -0,0 +1,247 @@
+export const userData = [
+  { 
+      name: 'Jan',
+      "Active Users": 2000, 
+  },
+
+  { 
+      name: 'Feb',
+      "Active Users": 5700, 
+  },
+
+  { 
+      name: 'Mar',
+      "Active Users": 3200, 
+  },
+
+  { 
+      name: 'Apr',
+      "Active Users": 2800, 
+
+  },
+
+  { 
+      name: 'May',
+      "Active Users": 4400, 
+  },
+
+  { 
+      name: 'Jun',
+      "Active Users": 2100, 
+  },
+
+  { 
+      name: 'Jul',
+      "Active Users": 7100, 
+  },
+
+  { 
+      name: 'Aug',
+      "Active Users": 4000, 
+  },
+
+  { 
+      name: 'Sep',
+      "Active Users": 6000, 
+  },
+
+  { 
+      name: 'Oct',
+      "Active Users": 5500, 
+  },
+
+  { 
+      name: 'Nov',
+      "Active Users": 4900, 
+  },
+
+  { 
+      name: 'Dec',
+      "Active Users": 7000, 
+  },
+
+];
+
+
+
+export const userRows = [
+  { 
+    id: 1, 
+    username: 'John Snow', 
+    avatar: "images/admin.jpg",
+    email: "john@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  { 
+    id: 2, 
+    username: 'John Snow', 
+    avatar: "images/admin.jpg",
+    email: "john@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  { 
+    id: 3, 
+    username: 'John Snow', 
+    avatar: "images/admin.jpg",
+    email: "john@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  { 
+    id: 4, 
+    username: 'John Snow', 
+    avatar: "images/admin.jpg",
+    email: "john@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  { 
+    id: 5, 
+    username: 'John Snow', 
+    avatar: "images/admin.jpg",
+    email: "john@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  { 
+    id: 6, 
+    username: 'John Snow', 
+    avatar: "images/admin.jpg",
+    email: "john@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  { 
+    id: 7, 
+    username: 'John Snow', 
+    avatar: "images/admin.jpg",
+    email: "john@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  { 
+    id: 8, 
+    username: 'John Snow', 
+    avatar: "images/admin.jpg",
+    email: "john@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  { 
+    id: 9, 
+    username: 'John Snow', 
+    avatar: "images/admin.jpg",
+    email: "john@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+  { 
+    id: 10, 
+    username: 'John Snow', 
+    avatar: "images/admin.jpg",
+    email: "john@gmail.com",
+    status: "active",
+    transaction: "$120.00",
+  },
+
+];
+
+export const productRows = [
+  {
+    id: 1,
+    name: "Ear Buds",
+    img:"images/airpods.png",
+    stock: 123,
+    status: "active",
+    price: "$120.00",
+  },
+  {
+    id: 2,
+    name: "Ear Buds",
+    img:"images/airpods.png",
+    stock: 123,
+    status: "active",
+    price: "$120.00",
+  },
+  {
+    id: 3,
+    name: "Ear Buds",
+    img:"images/airpods.png",
+    stock: 123,
+    status: "active",
+    price: "$120.00",
+  },
+  {
+    id: 4,
+    name: "Ear Buds",
+    img:"images/airpods.png",
+    stock: 123,
+    status: "active",
+    price: "$120.00",
+  },
+  {
+    id: 5,
+    name: "Ear Buds",
+    img:"images/airpods.png",
+    stock: 123,
+    status: "active",
+    price: "$120.00",
+  },
+  {
+    id: 6,
+    name: "Ear Buds",
+    img:"images/airpods.png",
+    stock: 123,
+    status: "active",
+    price: "$120.00",
+  },
+  {
+    id: 7,
+    name: "Ear Buds",
+    img:"images/airpods.png",
+    stock: 123,
+    status: "active",
+    price: "$120.00",
+  },
+  {
+    id: 8,
+    name: "Ear Buds",
+    img:"images/airpods.png",
+    stock: 123,
+    status: "active",
+    price: "$120.00",
+  },
+  {
+    id: 9,
+    name: "Ear Buds",
+    img:"images/airpods.png",
+    stock: 123,
+    status: "active",
+    price: "$120.00",
+  },
+  {
+    id: 10,
+    name: "Ear Buds",
+    img:"images/airpods.png",
+    stock: 123,
+    status: "active",
+    price: "$120.00",
+  },
+];
+
+export const productData = [
+  {
+    name: "Jan",
+    "Sales": 4000,
+  },
+  {
+    name: "Feb",
+    "Sales": 3000,
+  },
+  {
+    name: "Mar",
+    "Sales": 5000,
+  },
+];
